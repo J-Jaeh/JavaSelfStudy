7947f9d6134db96b0ea0183f9c140bea0247533b
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-11-20 15:02:09 +0900

    Initial commit

---
+++ polymorphsim_practice.java
@@ -0,0 +1,18 @@
+public class polymorphsim_practice {
+}
+
+/*
+다형성이란... 다향한 모습을 갖는것..
+한객체가 다양한 타입을 갖는것
+ >한 객체가 여러 타입의 기능을제공
+ >타입 상속으로 다형성 구현...
+ */
+
+public class Plug{
+    public void on(){
+        System.out.println("Plug on");
+    }
+    public void off(){
+
+    }
+}
